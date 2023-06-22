54bc54ba5091628b3e3df8384c8a89f8d11249e1
Ana Luiza Farias <125582400+analuiza-otd@users.noreply.github.com>
2023-06-22 17:02:21 -0300

    finalizando desafio

--- app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActivity.kt
@@ -42,6 +42,14 @@ class ListaProdutosActivity : AppCompatActivity() {
     private fun configuraRecyclerView() {
         val recyclerView = binding.activityListaProdutosRecyclerView
         recyclerView.adapter = adapter
+        adapter.quandoClicaNoItem = {
+            val intent = Intent(
+                this,
+                DetalhesProdutoActivity::class.java
+            ).apply {
+                putExtra(CHAVE_PRODUTO, it)
+            }
+            startActivity(intent)
+        }
     }
-
 }
